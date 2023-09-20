b2f984e069c5dd7173a0a305eafb9cd91ce772e9
chowie <christopher.howie@gmail.com>
2023-09-20 20:58:54 +0100

    Added and wired up GuesInput component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import GuessInput from "../GuessInput/GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+    const [guess, setGuess] = React.useState("");
+
+    function handleSubmitGuess(event) {
+        console.log(guess);
+    }
+    return (
+        <GuessInput
+            guess={guess}
+            setGuess={setGuess}
+            handleSubmitGuess={handleSubmitGuess}
+        />
+    );
 }
 
 export default Game;
