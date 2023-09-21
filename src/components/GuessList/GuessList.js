7e72baf421309e7ab390142bf08e228ca49ed5d7
chowie <christopher.howie@gmail.com>
2023-09-21 13:20:48 +0100

    Alternative method using a single loop

--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -7,7 +7,26 @@ function GuessList({ guessList }) {
     return (
         <>
             <div className="guess-results">
-                {guessList.map(({ id, value }) => (
+                {/* 
+                {
+                  How would I achieve this using a single loop?
+                  we would loop over the range from 0 to the limit
+                  but then we need to only include a Guess with a value
+                  if a value exists?
+                  Seems very inefficent and confusing.
+                }
+                */}
+                {range(0, NUM_OF_GUESSES_ALLOWED).map((num) => (
+                    <Guess
+                        key={
+                            guessList[num]
+                                ? guessList[num].id
+                                : crypto.randomUUID()
+                        }
+                        value={guessList[num] ? guessList[num].value : "     "}
+                    ></Guess>
+                ))}
+                {/* {guessList.map(({ id, value }) => (
                     <Guess
                         key={id}
                         value={value}
@@ -18,7 +37,7 @@ function GuessList({ guessList }) {
                         key={crypto.randomUUID()}
                         value={"     "}
                     ></Guess>
-                ))}
+                ))} */}
             </div>
         </>
     );
