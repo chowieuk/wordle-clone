08c6a97e3175621611a443afc2f8a7e356e19b7d
chowie <christopher.howie@gmail.com>
2023-09-23 17:26:00 +0100

    finished implementing baseline UI tests

---
+++ tests/baselineUI.spec.js
@@ -0,0 +1,131 @@
+// @ts-check
+import { test, expect } from "@playwright/experimental-ct-react";
+import { NUM_OF_GUESSES_ALLOWED, UNUSED_KEYBOARD } from "../src/constants";
+
+test.use({ viewport: { width: 700, height: 1200 } });
+
+test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    // TODO: replace with ENV variable?
+    await page.goto("http://localhost:1234");
+});
+
+test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle("Word Game");
+});
+
+test("header visible", async ({ page }) => {
+    const header = page.locator("h1");
+    await expect(header).toBeVisible();
+    await expect(header).toContainText("Word Game");
+});
+
+test("all guess list is visible", async ({ page }) => {
+    const guessList = page.locator(".guess-results");
+    await expect(guessList).toBeVisible();
+});
+
+test("all guesses are visible", async ({ page }) => {
+    const guesses = await page.locator(".guess").all();
+    for (let guess of guesses) {
+        await expect(guess).toBeVisible();
+    }
+});
+
+test("appropriate number of guesses", async ({ page }) => {
+    const guesses = await page.locator(".guess").all();
+    await expect(guesses.length).toEqual(NUM_OF_GUESSES_ALLOWED);
+});
+
+test("all guess cells are visible", async ({ page }) => {
+    const cells = await page.locator(".cell").all();
+    for (let cell of cells) {
+        await expect(cell).toBeVisible();
+    }
+});
+
+test("appropriate number of guess cells", async ({ page }) => {
+    // Note: test assumes that the guess word length is always five
+    const cells = await page.locator(".cell").all();
+    expect(cells.length).toEqual(NUM_OF_GUESSES_ALLOWED * 5);
+});
+
+test("guess input visible", async ({ page }) => {
+    const input = page.getByLabel("Enter guess:");
+    await expect(input).toBeVisible();
+});
+
+test("guess input editable", async ({ page }) => {
+    const input = page.getByLabel("Enter guess:");
+    await expect(input).toBeEditable();
+});
+
+test("guess input empty on mount", async ({ page }) => {
+    const input = page.getByLabel("Enter guess:");
+    await expect(input).toBeEmpty();
+});
+
+test("guess input field focused on mount", async ({ page }) => {
+    const input = page.getByLabel("Enter guess:");
+    await expect(input).toBeFocused();
+});
+
+test("keyboard visible", async ({ page }) => {
+    const keyboard = page.locator(".keyboard-wrapper");
+    await expect(keyboard).toBeVisible();
+});
+
+test("keyboard contains three keyboard rows", async ({ page }) => {
+    const keyboardRows = await page.locator(".keyboard-row").all();
+    expect(keyboardRows.length).toEqual(3);
+});
+
+test("there is a visible keyCell for each entry in UNUSED_KEYBOARD", async ({
+    page,
+}) => {
+    const cells = await page.locator(".keyCell").all();
+
+    expect(cells.length).toEqual(Object.keys(UNUSED_KEYBOARD).length);
+
+    for (let cell of cells) {
+        await expect(cell).toBeVisible();
+    }
+});
+
+test("there is a keyCell with the appropriate letter for each entry in UNUSED_KEYBOARD", async ({
+    page,
+}) => {
+    for (const key in Object.keys(UNUSED_KEYBOARD)) {
+        expect(page.locator(".keyCell", { hasText: key }));
+    }
+});
+
+test("the keyCells are rendered in the same order as they are entered within UNUSED_KEYBOARD", async ({
+    page,
+}) => {
+    const cells = await page.locator(".keyCell").all();
+
+    // Fetch the inner text from each cell using map and Promise.all
+    const cellTexts = await Promise.all(
+        cells.map(async (cell) => {
+            return cell.textContent();
+        })
+    );
+
+    // Compare with UNUSED_KEYBOARD keys
+    const keyboardKeys = Object.keys(UNUSED_KEYBOARD);
+    expect(cellTexts).toEqual(keyboardKeys);
+});
+
+test("the keyCells begin without status formatting", async ({ page }) => {
+    const keyCells = await page.locator(".keyCell").all();
+    const classNames = await Promise.all(
+        keyCells.map(async (cell) => {
+            return cell.getAttribute("class");
+        })
+    );
+
+    classNames.forEach((className) => {
+        expect(className).toEqual(" keyCell");
+    });
+});
---
+++ tests/plan.md
@@ -0,0 +1,53 @@
+Let's think about this from the top down
+
+-   App
+    -   Header
+    -   Game
+        -   GuessList
+        -   GuessInput
+        -   LossBanner
+        -   WonBanner
+        -   ResetGameButton
+
+While interesting, I'm not sure I strictly need tests on the component level.
+
+What consistent interface should the user see?
+
+-   a Header
+-   a guess results page (guess list), comprising of:
+    -   `NUM_OF_GUESSES_ALLOWED` guesses (rows)
+    -   each guess will have five cells
+-   a guess input box
+-   an onscreen keyboard
+
+What actions can a user take, and what outcomes can they expect?
+
+-   enter a guess
+
+    -   can input alphabetical characters
+    -   cannot submit a non five character guess
+    -   cannot enter more than five characters
+    -   cannot enter non-alphabetical characters
+
+-   submit a guess
+
+    -   guess input field is reset on submission
+    -   submitted guesses appears in the guess list
+        -   the letters of these guesses are appropriately formatted
+    -   the letters of the submited guesses are appropriately formatted in the onscreen keyboard
+    -   a submission matching the answer triggers the win banner
+        -   the win banner contains an accurate number of guesses taken
+        -   the win banner contains a reset button
+            -   the win banner contains no reset button if the user has cycled through all of the `WORDS`
+            -   `feature suggestion: ability to fully reset the game state at this point`
+    -   the user has up to `NUM_OF_GUESSES_ALLOWED` tries, after which they are presented with the loss banner if they have not entered a matching guess
+        -   the loss banner contains the `answer` that the user failed to guess
+        -   the loss banner contains a reset button
+        -   the loss banner contains no reset button if the user has cycled through all of the `WORDS`
+
+-   reset the game
+
+    -   the guess list an all its guesses are reset to its original state
+    -   a new target answer is chosen
+        -   this target answer should not be from one previouly guessed during this session
+    -   the formatting on the onscreen keyboard is reset
