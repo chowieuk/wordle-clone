08228942b5409def4c64e5cfa949461d5b1fc427
chowie <christopher.howie@gmail.com>
2023-09-24 19:05:15 +0100

    reached acceptable coverage for banner components

--- tests/banners.spec.js
+++ tests/banners.spec.js
@@ -7,6 +7,12 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 test.use({ viewport: { width: 700, height: 1200 } });
 
+test.beforeEach(async ({ page }) => {
+    // Go to the starting url before each test.
+    // TODO: replace with ENV variable?
+    await page.goto("http://localhost:1234");
+});
+
 test("a submission matching the answer triggers the win banner", async ({
     page,
 }) => {
@@ -27,4 +33,63 @@ test("the win banner contains the reset button", async ({ page }) => {
     await guessInput.press("Enter");
 
     await expect(page.getByRole("button", { name: "NEW GAME" })).toBeVisible();
+});
+
+test("the win banner contains an accurate number of guesses taken", async ({
+    page,
+}) => {
+    for (let i = 0; i < NUM_OF_GUESSES_ALLOWED; i++) {
+        const guessInput = page.getByLabel("Enter guess:");
+        const answer = await page.getByTestId("answerForTesting").innerText();
+        const expectedNumOfAnswers =
+            i === 0 ? "a single guess" : `${i + 1} guesses`;
+
+        for (let j = 0; j < i; j++) {
+            await guessInput.fill("XXXXX");
+            await guessInput.press("Enter");
+        }
+
+        await guessInput.fill(answer);
+        await guessInput.press("Enter");
+
+        await expect(page.locator(".happy.banner")).toContainText(
+            expectedNumOfAnswers
+        );
+        await page.getByRole("button", { name: "NEW GAME" }).click();
+    }
+});
+
+test("user has up to NUM_OF_GUESSES_ALLOWED incorrect attempts before triggering the loss banner", async ({
+    page,
+}) => {
+    const guessInput = page.getByLabel("Enter guess:");
+
+    for (let i = 0; i < NUM_OF_GUESSES_ALLOWED; i++) {
+        await guessInput.fill("XXXXX");
+        await guessInput.press("Enter");
+    }
+
+    await expect(page.locator(".sad.banner")).toBeVisible();
+});
+
+test("the loss banner contains the reset button", async ({ page }) => {
+    const guessInput = page.getByLabel("Enter guess:");
+
+    for (let i = 0; i < NUM_OF_GUESSES_ALLOWED; i++) {
+        await guessInput.fill("XXXXX");
+        await guessInput.press("Enter");
+    }
+
+    await expect(page.getByRole("button", { name: "NEW GAME" })).toBeVisible();
+});
+test("the loss banner contains the answer", async ({ page }) => {
+    const guessInput = page.getByLabel("Enter guess:");
+    const answer = await page.getByTestId("answerForTesting").innerText();
+
+    for (let i = 0; i < NUM_OF_GUESSES_ALLOWED; i++) {
+        await guessInput.fill("XXXXX");
+        await guessInput.press("Enter");
+    }
+
+    await expect(page.locator(".sad.banner")).toContainText(answer);
 });
