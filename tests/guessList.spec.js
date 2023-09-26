99d81dd086d0240ad6f36016340adce9e4e713e6
chowie <christopher.howie@gmail.com>
2023-09-26 10:35:17 +0100

    cleaned up unused functions / constants

--- tests/guessList.spec.js
+++ tests/guessList.spec.js
@@ -1,10 +1,7 @@
 // @ts-check
 import { test, expect } from "@playwright/experimental-ct-react";
-import { range } from "../src/utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../src/constants";
 import { generateTestGuess } from "./utils/helpers";
-
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 test.use({ viewport: { width: 700, height: 1200 } });
 
